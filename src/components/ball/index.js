2d56866b65ea6fdf4877a0b73d3f5ad09eb2b9a2
Pau Arjalaguer <pau.arjalaguer@gmail.com>
2024-05-05 18:18:43 +0200

    change background and sounds

--- src/components/ball/index.js
+++ src/components/ball/index.js
@@ -5,8 +5,8 @@ const Ball = (props) => {
         props.checkNumber(props.id);
 
     }
-    return (<div onClick={ballPress} key={props.id} className='w-1/3 text-center flex items-center justify-center '>
-        <div id={"ball_" + props.id} className='w-[40vh] h-[40vh] rounded-full bg-slate-100 flex items-center justify-center text-4xl font-bold'
+    return (<div onClick={ballPress} key={props.id} className='w-1/3 text-center flex items-center justify-center  '>
+        <div id={"ball_" + props.id} className='w-[40vh] h-[40vh] rounded-full bg-slate-100 flex items-center justify-center text-4xl font-bold border-2 shadow-2xl border-slate-400'
 
         >{props.id}</div>
     </div>
--- src/components/screen/index.js
+++ src/components/screen/index.js
@@ -15,20 +15,31 @@ const Screen = () => {
             if (id == activeNumber) {
                 generateNumber(1, number);
                 setScore(score + 1);
+                new Audio('./correct.mp3').play()
+            }else{
+                new Audio('./error.mp3').play()
             }
         }
     }
     const generateNumber = (min, max) => {
-
+        const colors = [null, 'bg-blue-200', 'bg-blue-300', 'bg-blue-400', 'bg-blue-500', 'bg-blue-600', 'bg-blue-800'];
         if (activeNumber != 0) {
             document.getElementById("ball_" + activeNumber).classList.add("bg-slate-100");
+            document.getElementById("ball_" + activeNumber).classList.remove("bg-blue-200");
+            document.getElementById("ball_" + activeNumber).classList.remove("bg-blue-300");
             document.getElementById("ball_" + activeNumber).classList.remove("bg-blue-400");
+            document.getElementById("ball_" + activeNumber).classList.remove("bg-blue-500");
+            document.getElementById("ball_" + activeNumber).classList.remove("bg-blue-600");
+            document.getElementById("ball_" + activeNumber).classList.remove("bg-blue-800");
+
         }
         let random = Math.floor(Math.random()
             * (max - min + 1)) + min;
+        let randomC = Math.floor(Math.random()
+            * (max - min + 1)) + min;
         setActiveNumber(random);
         document.getElementById("ball_" + random).classList.remove("bg-slate-100");
-        document.getElementById("ball_" + random).classList.add("bg-blue-400");
+        document.getElementById("ball_" + random).classList.add(colors[randomC]);
 
     };
 
@@ -46,16 +57,16 @@ const Screen = () => {
             setTime((time) => {
                 if (time === 0) {
                     clearInterval(timer);
-                    document.getElementById("screen").style.display="none";
+                    document.getElementById("screen").style.display = "none";
                     return 0;
-                   
+
                 } else return time - 1;
             });
         }, 1000);
     }, []);
     return (
-        <div className='bg-slate-700 w-full h-screen'>
-            <div className='font-bold text-white px-6 py-2'>Punts: {score} Temps: {time}</div>
+        <div className='bg-white w-full h-screen'>
+            <div className='font-bold text-slate-700 px-6 py-2 bg-slate-400 rounded-b-xl shadow-xl '>Punts: {score} -  Temps: {time}</div>
             <div className='flex flex-wrap h-screen' id="screen">
                 {balls.map((b) => {
                     return (<Ball setActiveNumber={setActiveNumber} checkNumber={checkNumber} key={b} id={b}></Ball>)
